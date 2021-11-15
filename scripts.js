2ea48ebaddb4a93585fa4d468613293abf08db26
Fawzi Najjar <fawzinajjar@icloud.com>
2021-11-15 19:11:04 +0200

    alerts update

--- scripts.js
+++ scripts.js
@@ -25,6 +25,7 @@ function postrequest(data) {
 
 // Submit Data
 function submitURL() {
+  document.getElementById('loading').innerText = 'Loading ...';
   let url_data = document.getElementById('1').value;
   postrequest(url_data);
 }
@@ -32,11 +33,40 @@ function submitURL() {
 // Add Data To The Main Page
 function addData(result) {
   let result2 = JSON.parse(result);
+  // Show Results DIV
+  // document.getElementById('result_container').innerText = '';
+  // document.getElementById('result_container').style.display = 'block';
+  document.getElementById('result_container').style.display = 'unset';
   document.getElementById('result_text').value = result2.shortenedURL;
+  navigator.clipboard.writeText(result2.shortenedURL);
+  document.getElementById('1').style.pointerEvents = 'none';
+  document.getElementById('submit').style.display = 'none';
+  document.getElementById('resubmit').style.display = 'unset';
+  document.getElementById('1').style.backgroundColor = 'ghostwhite';
+  document.getElementById('loading').innerText = 'Successful !';
+  window.setTimeout(function () {
+    document.getElementById('loading').style.display = 'none';
+  }, 3000);
 }
 
-// viewer
+// new URL
+function newURL() {
+  document.getElementById('1').style.pointerEvents = 'unset';
+  document.getElementById('result_container').style.display = 'none';
+  document.getElementById('1').value = '';
+  document.getElementById('1').style.backgroundColor = 'white';
+  document.getElementById('resubmit').style.display = 'none';
+  document.getElementById('submit').style.display = 'unset';
+}
+// Copy Link Button
 function copyText() {
   let data = document.getElementById('result_text');
   navigator.clipboard.writeText(data.value);
+  document.getElementById('loading').innerText = '';
+  document.getElementById('loading').style.display = 'block';
+  document.getElementById('loading').innerText = 'Linked Copied !';
+  window.setTimeout(function () {
+    document.getElementById('loading').style.display = 'none';
+  }, 3000);
 }
+function msgAlert() {}
